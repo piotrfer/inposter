75a4ddc2e47e9e8b2f4b37acae8629f18a09e340
piotrfer <piotrek99f@gmail.com>
2020-10-30 11:57:17 +0100

    sender-signup html

--- src/app.py
+++ src/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/sender/sign-up')
+def sender_signup():
+    return render_template('sender-signup.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
